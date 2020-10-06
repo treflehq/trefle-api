5dfed5803934811159dd7fef792d2002f26f095f
André Aubin <andre@andral.xyz>
2020-10-06 11:37:55 +0200

    Fix: separate multiple values with comma in data dump

--- scripts/dump-data.sh
+++ scripts/dump-data.sh
@@ -4,4 +4,4 @@
 currdate=$(date +%Y%m%d)
 rake dump:data
 (cd ../trefle-data && git add . && git commit -m "Dump from $currdate")
-(cd ../trefle-data && git pull origin master && git push origin master && gh release create "1.0.$currdate" 'species.csv#Trefle data' -t "1.0.$currdate" -n "This dump has been generated on $(date +%Y/%m/%d)")
+(cd ../trefle-data && git pull origin master && git push origin master && gh release create "1.0.0-alpha+$currdate" 'species.csv#Trefle data' -t "1.0.0-alpha+$currdate" -n "This dump has been generated on $(date +%Y/%m/%d)")
