b55ac6a4b63b066c11f68bc143d3b4840e0c90ae
André Aubin <andre@andral.xyz>
2020-10-16 13:06:35 +0200

    Use https

--- lib/migrators/images_fixer.rb
+++ lib/migrators/images_fixer.rb
@@ -9,17 +9,29 @@ module Migrators
 
       return if sp.species_images.count.zero? && sp.main_image_url.nil?
 
-      sp.main_image_url = nil unless HTTParty.get(sp.main_image_url).ok?
+      sp.main_image_url = HTTParty.get(uri_for(sp.main_image_url)).ok? ? uri_for(sp.main_image_url) : nil
 
-      sp.species_images.each do |si|
-        si.delete unless HTTParty.get(si.image_url).ok?
-      end
+      filter_species_images!(sp)
 
       if sp.main_image_url.nil?
         candidate = sp.species_images.order(score: :desc)&.reload&.first&.image_url
-        sp.main_image_url = candidate
+        sp.main_image_url = uri_for(candidate)
       end
       sp.save
+    end
+
+    def self.filter_species_images!(sp)
+      sp.species_images.each do |si|
+        if HTTParty.get(uri_for(si.image_url)).ok?
+          si.image_url = uri_for(si.image_url) 
+        else
+          si.delete
+        end
+      end
+    end
+
+    def self.uri_for(link)
+      link&.gsub('http://', 'https://')&.gsub(' ', '%20')
     end
   end
 end
