d0765173b3b12ca438141ef69f58e52f536e8b97
André Aubin <andre.aubin@futuragaia.com>
2022-12-11 22:03:58 +0100

    Limit transactions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,10 +5,6 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
-  # config.environments = %w[production]
+  config.traces_sample_rate = 0.01
+
 end
