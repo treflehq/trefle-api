a0c70355acaa1e47c7ef9a0130ef7129efeac556
André Aubin <andre@andral.xyz>
2025-09-30 18:28:13 +0200

    feat: Github links

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,29 +2,61 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     auth = request.env['omniauth.auth']
 
-    # On cherche d'abord si un utilisateur a déjà lié ce compte GitHub
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    # Find or create a user based on GitHub auth data
+    user = find_user_for_github_oauth(auth)
 
-    if user
-      # Si l'utilisateur existe déjà, on le connecte
+    if user.persisted?
+      # If the user was found (and potentially updated), sign them in
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     elsif current_user
-      # Si un utilisateur est déjà connecté (via Devise), on LIE son compte
+      # If a user is already signed in, link their GitHub account
       current_user.update(
         provider: auth.provider,
         uid: auth.uid,
         github_username: auth.info.nickname
       )
-      redirect_to edit_user_registration_path, notice: 'Your GitHub account has been linked.'
+      redirect_to edit_user_registration_path, notice: 'Your GitHub account was successfully linked.'
     else
-      # Cas non géré : un nouvel utilisateur essaie de se connecter via GitHub
-      # sans avoir de compte. Tu peux choisir de créer un compte ici ou de refuser.
-      redirect_to new_user_session_path, alert: 'No account found. Please sign up first.'
+      # If the user could not be created, handle validation errors
+      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
     end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to root_path, alert: "Could not authenticate you from GitHub."
   end
+
+  private
+
+  def find_user_for_github_oauth(auth)
+    # Find a user by provider/uid
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    return user if user
+
+    # Find a user by email (in case they signed up manually before)
+    user = User.find_by(email: auth.info.email)
+    if user
+      # If found by email, update their provider/uid for future sign-ins
+      user.update(provider: auth.provider, uid: auth.uid, github_username: auth.info.nickname)
+      return user
+    end
+
+    # If no user is found, create a new one
+    User.create do |new_user|
+      new_user.provider = auth.provider
+      new_user.uid = auth.uid
+      new_user.github_username = auth.info.nickname
+      new_user.email = auth.info.email
+      new_user.name = auth.info.name
+      # Devise requires a password, even for OAuth.
+      # We generate a random, secure one.
+      new_user.password = Devise.friendly_token[0, 20]
+
+      # Since the email is verified by GitHub, we can skip Devise's confirmation step.
+      # new_user.skip_confirmation!
+    end
+  end
+
 end
--- lib/sponsorship/github_sponsorship.rb
+++ lib/sponsorship/github_sponsorship.rb
@@ -2,21 +2,37 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class GithubSponsorshipService
+class Sponsorship::GithubSponsorship
   # Le 'login' est celui de l'organisation ou du compte qui reçoit les sponsors
   SPONSORED_ACCOUNT_LOGIN = 'treflehq'
   GITHUB_API_URL = URI.parse('https://api.github.com/graphql')
   # Tu dois générer un Personal Access Token (PAT) avec les droits `read:user` et `read:org`
   GITHUB_TOKEN = ENV['GITHUB_API_TOKEN']
 
-  def self.is_user_sponsoring?(user_github_login)
+  def self.get_sponsors()
     query = <<~GRAPHQL
       query {
-        user(login: "#{user_github_login}") {
-          sponsorshipForViewerAsSponsorable(sponsorableLogin: "#{SPONSORED_ACCOUNT_LOGIN}") {
-            tier {
-              name
-              monthlyPriceInDollars
+        organization(login: "#{SPONSORED_ACCOUNT_LOGIN}") {
+          sponsorshipsAsMaintainer(first: 100, activeOnly: true) {
+
+            nodes {
+              sponsorEntity {
+                # Check if it's an Organization and get its login
+                ... on Organization {
+                  login
+                }
+                # Check if it's a User and get its login
+                ... on User {
+                  login
+                  email
+                }
+              }
+              createdAt
+              tier {
+                name
+                description
+                monthlyPriceInDollars
+              }
             }
           }
         }
@@ -31,13 +47,30 @@ class GithubSponsorshipService
     request.body = { query: query }.to_json
 
     response = http.request(request)
+
+    puts response.inspect
     data = JSON.parse(response.body)
 
+    puts data.inspect
+
     # Navigue dans la structure de la réponse GraphQL
-    sponsorship = data.dig('data', 'user', 'sponsorshipForViewerAsSponsorable')
+    sponsorship = data.dig('data', 'organization', 'sponsorshipsAsMaintainer', 'nodes')
 
-    return nil unless sponsorship # L'utilisateur n'est pas un sponsor
+    return nil unless sponsorship&.any? # L'utilisateur n'est pas un sponsor
 
-    sponsorship['tier'] # Retourne les infos du tier, ex: {"name"=>"Pro", "monthlyPriceInDollars"=>10}
+    sponsorship.map{|e| [e["sponsorEntity"]["login"], e["tier"]["monthlyPriceInDollars"]] }.to_h
   end
+
+  def self.reassign_sponsor_status
+    sponsors = get_sponsors()
+    return unless sponsors
+
+    User.where.not(sponsored_tier: nil, github_username: sponsors.keys).update_all(sponsored_tier: nil)
+
+    sponsors.each do |github_login, tier_amount|
+      puts "Adding #{github_login} with tier #{tier_amount}"
+      User.find_by(github_username: github_login)&.update(sponsored_tier: tier_amount)
+    end
+  end
+
 end
