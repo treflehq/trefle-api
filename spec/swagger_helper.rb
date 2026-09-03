24f7eec5b266c33da02d6601a24c4bf58ef07533
André Aubin <andre@andral.xyz>
2026-09-03 12:32:13 +0200

    Move rswag configuration to the openapi_* names (#201)
    rswag 2.17 is the latest release; the swagger_root/swagger_docs/
    swagger_format setters it deprecates (removed in 3.0) are the last
    deprecation warnings the suite printed. Renamed to openapi_root/
    openapi_specs/openapi_format; rswag:specs:swaggerize still writes
    swagger/v1/swagger.yaml.

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -6,7 +6,7 @@ RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
-  config.swagger_root = Rails.root.join('swagger').to_s
+  config.openapi_root = Rails.root.join('swagger').to_s
 
   # Define one or more Swagger documents and provide global metadata for each one
   # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
@@ -14,7 +14,7 @@ RSpec.configure do |config|
   # By default, the operations defined in spec files are added to the first
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
-  config.swagger_docs = {
+  config.openapi_specs = {
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
       info: {
@@ -51,8 +51,8 @@ RSpec.configure do |config|
   }
 
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
-  # The swagger_docs configuration option has the filename including format in
+  # The openapi_specs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.swagger_format = :yaml
+  config.openapi_format = :yaml
 end
